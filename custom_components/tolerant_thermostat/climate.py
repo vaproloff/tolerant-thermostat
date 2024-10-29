feb1d2501954060a61a4d50763d567e6159f7e31
Konstantin Pivnov <vaproloff@ya.ru>
2024-10-29 17:29:51 +0300

    added thermostat adding callback with signing on target sensor and switch device changes

--- custom_components/tolerant_thermostat/climate.py
+++ custom_components/tolerant_thermostat/climate.py
@@ -34,6 +34,9 @@ from homeassistant.core import (
 )
 from homeassistant.exceptions import ConditionError
 from homeassistant.helpers import condition
+from homeassistant.helpers.event import (
+    async_track_state_change_event,
+)
 from homeassistant.helpers.restore_state import RestoreEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -101,6 +104,21 @@ class TolerantThermostat(ClimateEntity, RestoreEntity):
         else:
             self._attr_preset_modes = [PRESET_NONE]
         self._presets = presets
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added."""
+        await super().async_added_to_hass()
+
+        self.async_on_remove(
+            async_track_state_change_event(
+                self.hass, [self.sensor_entity_id], self._async_sensor_changed
+            )
+        )
+        self.async_on_remove(
+            async_track_state_change_event(
+                self.hass, [self.heater_entity_id], self._async_switch_changed
+            )
+        )
 
     @property
     def precision(self) -> float:
